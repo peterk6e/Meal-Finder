f781362ec740b8d6978e9d9e64c2e1b6a2660911
pierrek6e <pierrecass75@gmail.com>
2022-04-29 10:50:16 +0700

    random search

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const baseUrl =
-  "https://lit-earth-42250.herokuapp.com/https://www.themealdb.com/api/json/v1/1/search.php";
+  "https://lit-earth-42250.herokuapp.com/https://www.themealdb.com/api/json/v1/1/";
 
 const inputSearch = document.getElementById("search-input");
 const resultsTitle = document.getElementById("results-title");
@@ -9,15 +9,12 @@ const instructions = document.getElementById("instructions");
 const cards = document.getElementById("cards");
 const selectedMeal = document.getElementById("selected-meal");
 
-// random
-// www.themealdb.com/api/json/v1/1/random.php
-
 function search() {
   init();
   cards.innerText = "";
   let searched = inputSearch.value;
 
-  fetch(baseUrl + "?s=" + searched) //Arrabiata
+  fetch(baseUrl + "search.php?s=" + searched) //Arrabiata
     .then((response) => {
       if (response.status !== 200) {
         console.log("there was a problem. Status Code: " + response.status);
@@ -26,6 +23,27 @@ function search() {
       response.json().then((data) => {
         console.log(data.meals);
         updateResults(data);
+      });
+    })
+    .catch(function (err) {
+      console.log("Fetch Error", err);
+    });
+}
+
+function randomSearch() {
+  init();
+  cards.innerText = "";
+  let searched = inputSearch.value;
+
+  fetch(baseUrl + "/random.php")
+    .then((response) => {
+      if (response.status !== 200) {
+        console.log("there was a problem. Status Code: " + response.status);
+        return;
+      }
+      response.json().then((data) => {
+        console.log(data.meals);
+        showSelectedMeal(data.meals[0]);
       });
     })
     .catch(function (err) {
@@ -45,8 +63,8 @@ function updateIngredients(meal) {
   for (let i = 1; i <= 20; i++) {
     if (meal[`strIngredient${i}`]) {
       let ing = document.createElement("div");
-      ing.innerText = 
-      `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]
+      ing.innerText = `${meal[`strIngredient${i}`]} - ${
+        meal[`strMeasure${i}`]
       }`;
       ing.className = "ingredient";
       ingredients.appendChild(ing);
